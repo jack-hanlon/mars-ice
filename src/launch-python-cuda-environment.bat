ce7d977264045b63c28d117b3e7b93425d5e78f3
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-08-29 22:58:34 -0700

    launch batch file committed

---
+++ src/launch-python-cuda-environment.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Users\jackl\Anaconda3\Scripts\activate.bat" C:\Users\jackl\Anaconda3
+cmd
